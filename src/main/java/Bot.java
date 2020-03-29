c30514f1dc3057239649468261200897cfda8b4f
Pavel Chervyakov <asusp783@gmail.com>
2020-03-29 23:52:51 +0400

    create bot

---
+++ src/main/java/Bot.java
@@ -0,0 +1,66 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        System.getProperties().put( "proxySet", "true" );
+        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+        System.getProperties().put( "socksProxyPort", "9150" );
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if(message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "Привет":
+                    sendMsg(message, "Привет, Я показываю погоду.");
+                    break;
+                case "Как тебя зовут":
+                    sendMsg(message, "Меня зовут Паша.");
+                    break;
+                default:
+                    sendMsg(message, "У меня 2 функции:\n" +
+                            "Привет\n" +
+                            "Как тебя зовут");
+                    break;
+            }
+            if (message.equals("Привет")) {
+                sendMsg(message, "Привет, Я показываю погоду.");
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "WeatherAround";
+    }
+
+    public String getBotToken() {
+        return "986809256:AAHhIhKn6v5F9Zt0SWy4LkRY8kQV8ZO3sIs";
+    }
+}
